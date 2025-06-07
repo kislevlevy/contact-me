a059a62cbc91037f365abcf5ae247ece746ed448
Kislev Levy <kislev.levy@gmail.com>
2025-06-07 15:43:45 +0300

    V2.1

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -129,7 +129,7 @@ export const AdobeIcon: React.FC<IconProps> = ({ size = defaultSize }) => (
     viewBox="0 0 91 80"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_906_1839)">
+    <g clipPath="url(#clip0_906_1839)">
       <path d="M56.9686 0H90.4318V80L56.9686 0Z" fill="#EB1000" />
       <path d="M33.4632 0H0V80L33.4632 0Z" fill="#EB1000" />
       <path
